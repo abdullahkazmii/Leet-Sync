3bc09c45a587f129bb566731071ac36d8e3bff88
Abdullah <iamabdullah234@gmail.com>
2025-01-06 02:48:15 +0500

    Update Patterns

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -54,4 +54,66 @@ def pattern_7(n):
         print()
 
 
-pattern_7(5)
+def pattern_8(n):
+    for i in range(n):
+        for _ in range(i):
+            print(" ", end=" ")
+
+        for _ in range(2 * n - (2 * i + 1)):
+            print("*", end=" ")
+
+        for _ in range(i):
+            print(" ", end=" ")
+
+        print()
+
+
+def pattern_9(n):
+    for i in range(n):
+        for _ in range(n - i - 1):
+            print(" ", end=" ")
+
+        for _ in range(2 * i + 1):
+            print("*", end=" ")
+
+        for _ in range(n - i - 1):
+            print(" ", end=" ")
+
+        print()
+    for i in range(n):
+        for _ in range(i):
+            print(" ", end=" ")
+
+        for _ in range(2 * n - (2 * i + 1)):
+            print("*", end=" ")
+
+        for _ in range(i):
+            print(" ", end=" ")
+
+        print()
+
+
+def pattern_10(n):
+    for i in range(2 * n - 1):
+        stars = i + 1
+        if i >= n:
+            stars = 2 * n - i - 1
+        for _ in range(stars):
+            print("*", end=" ")
+        print()
+
+
+pattern_10(5)
+
+
+def pattern_11(n):
+    start = 1
+    for i in range(n):
+        start = 1 if (i % 2) == 0 else 0
+        for _ in range(i + 1):
+            print(start, end="")
+            start = 1 - start
+        print()
+
+
+pattern_11(5)
