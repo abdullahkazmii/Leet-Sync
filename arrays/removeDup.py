f29a48e2fd7b4a742510bff522be7efc9b9acbb7
Abdullah Kazmi <iamabdullah234@gmail.com>
2025-02-10 08:24:55 +0500

    Remove Duplicates from Sorted Array	Accepted	3 ms

--- arrays/removeDup.py
+++ arrays/removeDup.py
@@ -1,13 +1,26 @@
 from typing import List
 
 
-def removeDuplicates(nums: List[int]):
-    l = 1
-    for r in range(1, len(nums)):
-        if nums[r] != nums[r - 1]:
-            nums[l] = nums[r]
-            l += 1
-    return l,  nums
+# def removeDuplicates(nums: List[int]):
+#     l = 1
+#     for r in range(1, len(nums)):
+#         if nums[r] != nums[r - 1]:
+#             nums[l] = nums[r]
+#             l += 1
+#     return l,  nums
+
+
+
+
+
+def removeDuplicates(nums: List[int]) -> int:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums.pop(i + 1)
+            else:
+                i += 1
+        return len(nums)
 
 
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
